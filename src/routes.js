bdefedbc1eb180348e2427e23b6d752ce21ed84c
azertyDev <khikmatovumid@mail.ru>
2020-01-28 21:19:54 +0500

    Add cars component

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Users from "./Pages/Users/user";
+import Cars from "./Pages/Cars/cars";
 const Routes = () => {
   return (
     <Fragment>
       <Router>
         <Switch>
-          <Route path="/users" component={Users} />
+          <Route exact path="/users" component={Users} />
+          <Route exact path="/cars" component={Cars} />
         </Switch>
       </Router>
     </Fragment>
