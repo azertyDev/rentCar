493d05656635d8733ca046bd9ebc3b1b6636acdd
FeruzDev <feruzjalilov7314@gmail.com>
2020-01-29 15:45:41 +0300

    add login page

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Users from "./Pages/Users/user";
+import Login from "./Pages/Login/Login";
 const Routes = () => {
   return (
     <Fragment>
       <Router>
         <Switch>
           <Route path="/users" component={Users} />
+          <Route path="/login" component={Login} />
         </Switch>
       </Router>
     </Fragment>
